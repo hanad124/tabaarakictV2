10647eb58dc3220bc57a6eef10918a20eb5ab26b
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-08-12 15:36:33 +0300

    Modified hero section spaces for large screens

--- pages/Home/Hero.tsx
+++ pages/Home/Hero.tsx
@@ -15,22 +15,25 @@ const Hero = () => {
               "linear-gradient(126deg, rgba(13, 21, 34, 0.75) 0%, rgba(0, 39, 98, 0.47) 100%), linear-gradient(179deg, rgba(13, 21, 34, 0.90) 0%, rgba(0, 39, 98, 0.81) 98.98%), url('https://drive.google.com/uc?export=download&id=1O8BpNIrrF0aIA2onyIiBhT3dVYO4Sh5w')",
           }}
         >
-          <div className="pt-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-4">
+          <div className="pt-32 lg:pt-48 md:pt-44 max-w-7xl mx-auto px-4 sm:px-6 lg:px-4">
             <div className="flex flex-col md:flex-row md:justify-between items-center">
               <div className="md:w-1/2">
                 <h1
-                  className="lg:text-5xl  lg:mt-0 md:mt-0 text-4xl md:text-4xl sm:text-3xl leading-relaxed text-white font-bold mb-4 mt-6"
+                  className="lg:text-5xl  lg:mt-0 md:mt-0 text-4xl md:text-4xl sm:text-4xl leading-relaxed text-white font-bold mb-4 mt-6"
                   style={{ lineHeight: "138.5%" }}
                 >
                   Empower Your Business with Innovative ICT Solutions
                 </h1>
                 <p className="text-white mt-[2rem] md:mt-0 lg:mt-0 lg:text-lg md:text-lg text-md mb-8 leading-relaxed">
                   At{" "}
-                  <span className="font-semibold">`Tabaarak ICT Solutions`</span>,
-                  we are dedicated to transforming businesses with our advanced
-                  ICT solutions. Our focus is on delivering state-of-the-art
-                  technologies that ignite growth, optimize efficiency, and
-                  elevate customer experiences to unprecedented levels.
+                  <span className="font-semibold">
+                    `Tabaarak ICT Solutions`
+                  </span>
+                  , we are dedicated to transforming businesses with our
+                  advanced ICT solutions. Our focus is on delivering
+                  state-of-the-art technologies that ignite growth, optimize
+                  efficiency, and elevate customer experiences to unprecedented
+                  levels.
                 </p>
 
                 <Button
