29ba5a2eb5d834e95319412698241606999b4cf3
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-08-15 19:19:53 +0300

    Completed Testimonials section.

--- pages/Home/Clients.tsx
+++ pages/Home/Clients.tsx
@@ -2,12 +2,22 @@
 
 import { useState, useEffect } from "react";
 import { clients } from "@/data";
-
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import React from "react";
 
 const Clients = () => {
   const [client, setClient] = useState(clients[0]);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating image loading delay
+    const timer = setTimeout(() => {
+      setClient(clients[activeIndex]);
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   const handleClick = (index: React.SetStateAction<number>) => {
     setActiveIndex(index);
@@ -28,20 +38,24 @@ const Clients = () => {
             solutions
           </p>
 
-          <div className="mt-16 flex flex-col justify-between md:flex-row">
+          <div className="mt-16 flex flex-col justify-between items-center lg:flex-row">
             <div className="lg:w-2/5">
               <div className="flex justify-center lg:justify-start w-full">
-                <img
-                  src="https://drive.google.com/uc?export=download&id=1LV5pkMGtPPMOFdmB3V0BN2GTObnPWi-o"
-                  alt="quote icon"
-                  className="w-24"
-                />
+                {isLoading ? (
+                  <Skeleton circle={true} height={80} width={80} />
+                ) : (
+                  <img
+                    src="https://drive.google.com/uc?export=download&id=1LV5pkMGtPPMOFdmB3V0BN2GTObnPWi-o"
+                    alt="quote icon"
+                    className="w-24 md:w-36"
+                  />
+                )}
               </div>
-              <p className="text-custom_tertiary  text-lg lg:text-2xl  -mt-16" style={{lineClamp: "1.5rem"}}>
+              <p className="text-custom_tertiary leading-[34px] md:leading-[46px]   text-lg md:text-2xl  -mt-16">
                 {client.text}
               </p>
               <p className="text-custom_textColor font-sm mt-4">
-                <span className="text-custom_primary font-medium text-sm">
+                <span className="text-custom_primary font-medium text-lg">
                   {client.name}
                 </span>{" "}
                 {client.title}{" "}
@@ -53,7 +67,7 @@ const Clients = () => {
                 <div
                   className={
                     index === activeIndex
-                      ? "active ring ring-custom_primary rounded-md"
+                      ? "active ring-[2px] ring-custom_primary rounded-md scale-110"
                       : "opacity-50"
                   }
                   key={client.name}
@@ -65,7 +79,7 @@ const Clients = () => {
                   <img
                     src={client.img}
                     alt={client.name}
-                    className="w-40 h-36 max-h-36 rounded-md cursor-pointer"
+                    className="w-40 h-40 max-h-36 rounded-md cursor-pointer"
                   />
                 </div>
               ))}
