2844a9374829253dede0726296b6d75f73a1b01b
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-08-20 21:54:31 +0300

    Updated the UI of hero section.

--- pages/Home/Hero.tsx
+++ pages/Home/Hero.tsx
@@ -40,18 +40,18 @@ const Hero = () => {
       >
         {/* first slide */}
         <SwiperSlide>
-          <div className="bg-hero-image-1 bg-cover bg-center h-[50rem] overflow-hidden">
+          <div className="bg-hero-image-1 bg-cover bg-center h-[36rem] md:h-[50rem] overflow-hidden">
             <div className="container mx-auto px-4 py-8 h-screen flex justify-center items-center">
-              <div className="flex flex-col justify-center items-center mt-24 md:mt-0">
+              <div className="flex flex-col justify-center items-center  md:mt-0">
                 <div className="md:w-4/6">
                   <h1
-                    className="text-4xl md:text-6xl text-white font-bold mb-4 text-center"
+                    className="text-3xl md:text-6xl text-white font-bold mb-4 text-center"
                     style={{ lineHeight: "138.5%" }}
                   >
                     Empower Your Business with Innovative ICT Solutions
                   </h1>
                 </div>
-                <p className="text-white text-lg mb-8 lg:w-[681px] leading-[30px] text-center font-light">
+                <p className="text-white text-md  md:text-lg mb-8 lg:w-[681px] leading-[30px] text-center font-light">
                   At Tabaarak ICT Solutions, we are dedicated to transforming
                   businesses with our advanced ICT solutions. Our focus is on
                   delivering state-of-the-art technologies that ignite growth,
@@ -70,18 +70,18 @@ const Hero = () => {
         </SwiperSlide>
         {/* 2nd slide */}
         <SwiperSlide>
-          <div className="bg-hero-image-2 bg-cover bg-center h-[50rem]">
+          <div className="bg-hero-image-2 bg-cover bg-center h-[36rem] md:h-[50rem]">
             <div className="container mx-auto px-4 py-8 h-screen flex justify-center items-center">
-              <div className="flex flex-col justify-center items-center mt-24 md:mt-0">
+              <div className="flex flex-col justify-center items-center md:mt-0">
                 <div className="md:w-4/6">
                   <h1
-                    className="text-4xl md:text-6xl text-white font-bold mb-4 text-center"
+                    className="text-3xl md:text-6xl text-white font-bold mb-4 text-center"
                     style={{ lineHeight: "138.5%" }}
                   >
                     Empower Your Business with Innovative ICT Solutions
                   </h1>
                 </div>
-                <p className="text-white text-lg mb-8 lg:w-[681px] leading-[30px] text-center font-light">
+                <p className="text-white text-md md:text-lg mb-8 lg:w-[681px] leading-[30px] text-center font-light">
                   At Tabaarak ICT Solutions, we are dedicated to transforming
                   businesses with our advanced ICT solutions.
                 </p>
@@ -98,7 +98,7 @@ const Hero = () => {
       </Swiper>
 
       {/*Domain Search section  */}
-      <div className="domain-search-section -mt-[2rem] relative z-10">
+      <div className="domain-search-section -mt-6 md:-mt-[2rem] relative z-10">
         <div className="flex justify-between px-4 border-2 mx-auto md:w-4/6 lg:w-3/6 focus-within:ring-[3px] focus-within:ring-[#0b62e571] rounded-full w-5/6  border-custom_primary bg-white py-2">
           <div className="flex w-full items-center">
             <p className="border-r-2 border-[#DDD] text-[#323F7C] pr-2 hidden md:block">
@@ -115,7 +115,7 @@ const Hero = () => {
             <p className="hidden">Search</p>
           </div>
         </div>
-        <div className="flex gap-4 flex-wrap px-4 justify-center mt-10">
+        <div className="flex gap-4 flex-wrap px-4 justify-center mt-5 md:16">
           <p className="text-[#323F7C] font-semibold">
             <span className="text-[#0B63E5]">.so</span> $15/Year
           </p>
