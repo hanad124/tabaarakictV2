407922a7736280e19f58d1956894c087c2cf96a0
hanad124 <hanaddaahir124@gmail.com>
2023-09-05 14:59:39 +0300

    🔥  Handled filtering error & add custom message.

--- app/careers/(components)/SingleJob.tsx
+++ app/careers/(components)/SingleJob.tsx
@@ -105,71 +105,77 @@ const SingleJob: React.FC<ComponentProps> = ({ careers }) => {
       </div>
 
       <div className="flex flex-wrap items-center gap-5 mt-16">
-        {filteredCareers.map((career) => (
-          <div
-            key={career.slug}
-            className="border w-full md:w-[400px] rounded-lg p-5"
-          >
-            <Badge className="bg-[#F1EBFF] hover:bg-[#7534FF] hover:text-background text-[#7534FF] rounded-full uppercase px-3 py-1 cursor-pointer">
-              {career.category}
-            </Badge>
-            <Link href={`/careers/${career.slug}`} passHref>
-              <p className="text-custom_secondary text-lg font-semibold mt-3">
-                {career.name}
-              </p>
-            </Link>
-            <div className="flex items-center gap-2 mt-3">
-              <p className="text-sm text-custom_textColor">
-                Type:{" "}
-                <span className="text-custom_secondary text-base ">
-                  {career.type}
-                </span>
-              </p>
-              <div className="w-1 h-1 rounded-full bg-slate-300"></div>
-              <p className="text-sm text-custom_textColor">
-                Location:{" "}
-                <span className="text-custom_secondary text-base">
-                  {career.location}
-                </span>
-              </p>
-            </div>
-            {/* action buttons */}
-            <div className="flex gap-5 mt-7">
-              <Button className="group hover:bg-custom_primary text-background bg-custom_primary/10 text-custom_primary shadow-none flex items-center gap-2 py-5">
-                <span className="group-hover:text-background">Apply Now</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                >
-                  <path
-                    d="M3.125 10H16.875"
-                    stroke="#0B63E5"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className=" group-hover:stroke-background"
-                  />
-                  <path
-                    d="M11.25 4.375L16.875 10L11.25 15.625"
-                    stroke="#0B63E5"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="  group-hover:stroke-background"
-                  />
-                </svg>
-              </Button>
+        {filteredCareers.length === 0 ? (
+          <p className="text-custom_secondary text-center">
+            None of the available jobs match your specified filters.
+          </p>
+        ) : (
+          filteredCareers.map((career) => (
+            <div
+              key={career.slug}
+              className="border w-full md:w-[400px] rounded-lg p-5"
+            >
+              <Badge className="bg-[#F1EBFF] hover:bg-[#7534FF] hover:text-background text-[#7534FF] rounded-full uppercase px-3 py-1 cursor-pointer">
+                {career.category}
+              </Badge>
               <Link href={`/careers/${career.slug}`} passHref>
-                <Button className="bg-transparent hover:underline  hover:bg-transparent  text-custom_primary shadow-none">
-                  Read More
-                </Button>
+                <p className="text-custom_secondary text-lg font-semibold mt-3">
+                  {career.name}
+                </p>
               </Link>
+              <div className="flex items-center gap-2 mt-3">
+                <p className="text-sm text-custom_textColor">
+                  Type:{" "}
+                  <span className="text-custom_secondary text-base ">
+                    {career.type}
+                  </span>
+                </p>
+                <div className="w-1 h-1 rounded-full bg-slate-300"></div>
+                <p className="text-sm text-custom_textColor">
+                  Location:{" "}
+                  <span className="text-custom_secondary text-base">
+                    {career.location}
+                  </span>
+                </p>
+              </div>
+              {/* action buttons */}
+              <div className="flex gap-5 mt-7">
+                <Button className="group hover:bg-custom_primary text-background bg-custom_primary/10 text-custom_primary shadow-none flex items-center gap-2 py-5">
+                  <span className="group-hover:text-background">Apply Now</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M3.125 10H16.875"
+                      stroke="#0B63E5"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className=" group-hover:stroke-background"
+                    />
+                    <path
+                      d="M11.25 4.375L16.875 10L11.25 15.625"
+                      stroke="#0B63E5"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="  group-hover:stroke-background"
+                    />
+                  </svg>
+                </Button>
+                <Link href={`/careers/${career.slug}`} passHref>
+                  <Button className="bg-transparent hover:underline  hover:bg-transparent  text-custom_primary shadow-none">
+                    Read More
+                  </Button>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </>
   );
