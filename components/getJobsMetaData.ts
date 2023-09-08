efd79328029e6bddd57419500275b09ab6f502e1
hanad124 <hanaddaahir124@gmail.com>
2023-09-08 23:15:41 +0300

    Updated the auto deletion of the jobs file when the date expires and filtered out expired job posts without deleting the file.

--- components/getJobsMetaData.ts
+++ components/getJobsMetaData.ts
@@ -8,38 +8,38 @@ const getJobsMetadata = (): TJobs[] => {
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
   // Get file creation date for each file
-  const jobs: (TJobs | null)[] = markdownPosts.map((fileName) => {
-    const fileStats = fs.statSync(`jobs/${fileName}`);
-    const fileCreationDate = new Date(fileStats.birthtime); // Convert birthtime to Date object
+  const jobs: TJobs[] = markdownPosts
+    .map((fileName) => {
+      const fileStats = fs.statSync(`jobs/${fileName}`);
+      const fileCreationDate = new Date(fileStats.birthtime); // Convert birthtime to Date object
 
-    const fileContents = fs.readFileSync(`jobs/${fileName}`, "utf-8");
-    const matterResult = matter(fileContents);
+      const fileContents = fs.readFileSync(`jobs/${fileName}`, "utf-8");
+      const matterResult = matter(fileContents);
 
-    const issueDate = matterResult.data.issueDate;
-    const expireDate = matterResult.data.expireDate;
+      const issueDate = matterResult.data.issueDate;
+      const expireDate = matterResult.data.expireDate;
 
-    // Check if the job has expired
-    const currentDate = new Date();
-    const expirationDate = new Date(expireDate);
-    if (currentDate > expirationDate) {
-      // Delete the job post file
-      fs.unlinkSync(`jobs/${fileName}`);
-      return null; // Skip the job entry
-    }
+      // Check if the job has expired
+      const currentDate = new Date();
+      const expirationDate = new Date(expireDate);
+      if (currentDate > expirationDate) {
+        return null; // Skip the job entry
+      }
 
-    return {
-      slug: fileName.replace(".md", ""),
-      name: matterResult.data.name,
-      issueDate,
-      expireDate,
-      type: matterResult.data.type,
-      location: matterResult.data.location,
-      category: matterResult.data.category,
-      creationDate: fileCreationDate, // Store creation date as Date object
-    };
-  });
+      return {
+        slug: fileName.replace(".md", ""),
+        name: matterResult.data.name,
+        issueDate,
+        expireDate,
+        type: matterResult.data.type,
+        location: matterResult.data.location,
+        category: matterResult.data.category,
+        creationDate: fileCreationDate, // Store creation date as Date object
+      };
+    })
+    .filter(Boolean) as TJobs[];
 
-  return jobs.filter(Boolean) as TJobs[];
+  return jobs;
 };
 
 export default getJobsMetadata;
