7875af530791aca90beab423ffede7c3fb62c24d
hanad124 <hanaddaahir124@gmail.com>
2023-09-09 12:23:00 +0300

    feat(contact): Add responsive form with beautiful UI
    The contact page now includes a responsive form with an aesthetically pleasing user interface. The form is divided into two layouts, with the left side displaying contact information and the right side containing form fields. The form fields include name, email, subject, company, and message inputs. The form has been styled using Tailwind CSS to ensure a visually appealing and responsive design. This enhancement improves the user experience and makes it easier for visitors to get in touch with us.
    
    #contact #form #UI #responsive #TailwindCSS

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BreadCrumb from "@/components/BreadCrumb";
+import Form from "./(components)/Form";
 
 const Contact = () => {
   const breadcrumbItems = [{ label: "Home", link: "/" }];
@@ -7,6 +8,20 @@ const Contact = () => {
   return (
     <div className="bg-background min-h-screen">
       <BreadCrumb items={breadcrumbItems} title="Contact" />
+      <div className="py-16 container mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className=" text-custom_secondary text-3xl font-bold leading-normal lg:leading-relaxed lg:text-5xl md:text-4xl md:mx-24 text-center lg:mx-36 mb-0">
+          Contact Us
+        </h2>
+        <p className="text-custom_textColor my-3 leading-relaxed font-light mx-3 lg:mx-80 md:mx-36">
+          Thank you for your interest in{" "}
+          <span className="font-medium">Tabaarak ICT Solutions</span>. We
+          appreciate your feedback and inquiries. Please don't hesitate to
+          contact us using the information provided below. We look forward to
+          hearing from you.
+        </p>
+        {/* contact Form */}
+        <Form />
+      </div>
     </div>
   );
 };
